e46c6a83ca2ad669b2b9981d5d213bab310cb4d2
Hadoop <hadoop@hadoop-1545.(none)>
2013-05-31 17:40:16 +0530

    initialized rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleRailsApp::Application.config.secret_token = '8e1988635b8c4e42b5a7b17cbe13c327584f2e62e047a4612876a2af6316e532c847b86c4de20a4309ad1a15de69dfc40ffb7fffe070cb9e85deade73cd057a8'
